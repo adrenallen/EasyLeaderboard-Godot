9d5684a744855d3e1aed5e437f9d3d210719bee8
Garrett <garrettcoleallen@gmail.com>
2022-09-05 16:09:40 -0400

    trying to package scenes

--- addons/easyleaderboard/EasyLeaderboardPlugin.gd
+++ addons/easyleaderboard/EasyLeaderboardPlugin.gd
@@ -1,12 +1,8 @@
-tool
+@tool
 extends EditorPlugin
 
 func _enter_tree():
-	# Initialization of the plugin goes here.
-	add_custom_type("EasyLeaderboardAPI", "Node", preload("api/EasyLeaderboard.tscn"), preload("icon.png"))
-    add_custom_type("EasyLeaderboard", "Control", preload("leaderboard/Leaderboard.tscn"), preload("icon.png"))
+	pass
 
 func _exit_tree():
-	# Clean-up of the plugin goes here.
-	remove_custom_type("EasyLeaderboardAPI")
-    remove_custom_type("EasyLeaderboard")
+	pass
