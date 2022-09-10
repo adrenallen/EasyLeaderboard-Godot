a9544430fb223998332d1167ea8b73c11a31c57c
Garrett <garrettcoleallen@gmail.com>
2022-09-10 14:25:15 -0400

    work on converting to 3.5

--- addons/easyleaderboard/api/EasyLeaderboard.gd
+++ addons/easyleaderboard/api/EasyLeaderboard.gd
@@ -8,20 +8,20 @@ const DEFAULT_EASY_LEADERBOARD_URL = "https://lb.userdefined.io"
 const DEFAULT_GAME_NAME = "easy-leaderboard-example"
 const DEFAULT_SORT_RESULTS_ASCENDING = false
 
-@export var easy_leaderboard_url  : String = DEFAULT_EASY_LEADERBOARD_URL
-@export var game_name : String = DEFAULT_GAME_NAME
-@export var sort_results_ascending : bool = DEFAULT_SORT_RESULTS_ASCENDING
+export var easy_leaderboard_url  : String = DEFAULT_EASY_LEADERBOARD_URL
+export var game_name : String = DEFAULT_GAME_NAME
+export var sort_results_ascending : bool = DEFAULT_SORT_RESULTS_ASCENDING
 
-@export var page_size : int = 10
-@export var current_page : int = 1
+export var page_size : int = 10
+export var current_page : int = 1
 
-@onready var get_leaderboard_request : HTTPRequest = HTTPRequest.new()
-@onready var submit_score_request : HTTPRequest = HTTPRequest.new()
+onready var get_leaderboard_request : HTTPRequest = HTTPRequest.new()
+onready var submit_score_request : HTTPRequest = HTTPRequest.new()
 
 
 func _ready():
-	get_leaderboard_request.request_completed.connect(_on_get_leaderboard_request_request_completed)
-	submit_score_request.request_completed.connect(_on_submit_score_request_request_completed)
+	get_leaderboard_request.connect("request_completed", self, "_on_get_leaderboard_request_request_completed")
+	submit_score_request.connect("request_completed", self, "_on_submit_score_request_request_completed")
 	add_child(get_leaderboard_request)
 	add_child(submit_score_request)
 	
@@ -58,33 +58,31 @@ func _on_get_leaderboard_request_request_completed(_result, response_code, _head
 	
 	# TODO - handle a non-successful response code!
 	if response_code != 200:
-		push_error("Error in leaderboard results HTTP response. Got code ", response_code, " and body ", response_body)
+		push_error("Error in leaderboard results HTTP response. Got code " + str(response_code) + " and body " + str(response_body))
 		return
 	
-	var json = JSON.new()
-	var error = json.parse(response_body)
-	if error == OK:
-		leaderboard_results_changed.emit(json.get_data())
+	var json = JSON.parse(response_body)
+	if json:
+		emit_signal("leaderboard_results_changed", json.result)
 	else:
 		# TODO - handle json error!
-		push_error("JSON Parse Error: ", json.get_error_message(), " in ", response_body, " at line ", json.get_error_line())
+		push_error("JSON Parse Error: in " + response_body)
 
 
 func _submit_leaderboard_score(score_name, score_value, score_metadata = {}, score_validation = null):
-	var json = JSON.new()
 	var request = { name = score_name,
 		score = score_value,
 		game = game_name,
-		metaData = json.stringify(score_metadata)}
+		metaData = JSON.print(score_metadata)}
 		
 	# Validation is not _required_ so only send along if 
 	# we have a value to send
 	if score_validation:
 		request["validation"] = score_validation
 	else:
-		request["validation"] = json.stringify(request).sha256_text()
+		request["validation"] = JSON.print(request).sha256_text()
 
-	submit_score_request.request(easy_leaderboard_url + "/games/submit", ['Content-Type: application/json'], false, HTTPClient.METHOD_POST, json.stringify(request))
+	submit_score_request.request(easy_leaderboard_url + "/games/submit", ['Content-Type: application/json'], false, HTTPClient.METHOD_POST, JSON.print(request))
 
 
 func _on_submit_score_request_request_completed(_result, response_code, _headers, body):
@@ -93,14 +91,13 @@ func _on_submit_score_request_request_completed(_result, response_code, _headers
 	
 	# TODO - handle a non-successful response code!
 	if response_code != 200:
-		push_error("Error in submit score HTTP response. Got code ", response_code, " and body ", body.get_string_from_utf8())
+		push_error("Error in submit score HTTP response. Got code " + str(response_code) + " and body " + body.get_string_from_utf8())
 		return
 	
-	var json = JSON.new()
-	var error = json.parse(response_body)
-	if error == OK:
-		leaderboard_score_submitted.emit(json.get_data())
+	var json = JSON.parse(response_body)
+	if json:
+		emit_signal("leaderboard_score_submitted", json.result)
 	else:
 		# TODO - handle json error!
-		push_error("JSON Parse Error: ", json.get_error_message(), " in ", response_body, " at line ", json.get_error_line())
+		push_error("JSON Parse Error: in " + response_body)
 		
